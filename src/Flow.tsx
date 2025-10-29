b3fe6db184e873834760ec3dff563d79a6edfdc6
pepormaty <pepormaty@gmail.com>
2025-10-29 19:19:15 +0700

    sda

--- src/Flow.tsx
+++ src/Flow.tsx
@@ -19,9 +19,9 @@ const dagreGraph = new dagre.graphlib.Graph().setDefaultEdgeLabel(() => ({}));
 const nodeWidth = 172;
 const nodeHeight = 36;
 
-const getLayoutedElements = (nodes, edges, direction = 'TB') => {
-  const isHorizontal = direction === 'LR';
-  dagreGraph.setGraph({ rankdir: direction });
+const getLayoutedElements = (nodes, edges) => {
+ 
+  dagreGraph.setGraph({ rankdir: "TB" });
 
   nodes.forEach((node) => {
     dagreGraph.setNode(node.id, { width: nodeWidth, height: nodeHeight });
@@ -37,10 +37,6 @@ const getLayoutedElements = (nodes, edges, direction = 'TB') => {
     const nodeWithPosition = dagreGraph.node(node.id);
     const newNode = {
       ...node,
-      targetPosition: isHorizontal ? 'left' : 'top',
-      sourcePosition: isHorizontal ? 'right' : 'bottom',
-      // We are shifting the dagre node position (anchor=center center) to the top left
-      // so it matches the React Flow node anchor point (top left).
       position: {
         x: nodeWithPosition.x - nodeWidth / 2,
         y: nodeWithPosition.y - nodeHeight / 2,
@@ -65,23 +61,11 @@ export const Flow = () => {
   const onConnect = useCallback(
     (params) =>
       setEdges((eds) =>
-        addEdge({ ...params, type: ConnectionLineType.SmoothStep, animated: true }, eds),
+        addEdge({ ...params}, eds),
       ),
     [],
   );
-  const onLayout = useCallback(
-    (direction) => {
-      const { nodes: layoutedNodes, edges: layoutedEdges } = getLayoutedElements(
-        nodes,
-        edges,
-        direction,
-      );
 
-      setNodes([...layoutedNodes]);
-      setEdges([...layoutedEdges]);
-    },
-    [nodes, edges],
-  );
 
   return (
     <ReactFlow
@@ -93,14 +77,7 @@ export const Flow = () => {
       connectionLineType={ConnectionLineType.SmoothStep}
       fitView
     >
-      <Panel position="top-right">
-        <button className="xy-theme__button" onClick={() => onLayout('TB')}>
-          vertical layout
-        </button>
-        <button className="xy-theme__button" onClick={() => onLayout('LR')}>
-          horizontal layout
-        </button>
-      </Panel>
+     
       <Background />
     </ReactFlow>
   );
--- src/initialElements.ts
+++ src/initialElements.ts
@@ -1,6 +1,5 @@
 const position = { x: 0, y: 0 };
-const edgeType = 'smoothstep';
- 
+
 export const initialNodes = [
   {
     id: '1',
@@ -58,15 +57,15 @@ export const initialNodes = [
 ];
  
 export const initialEdges = [
-  { id: 'e12', source: '1', target: '2', type: edgeType, animated: true },
-  { id: 'e13', source: '1', target: '3', type: edgeType, animated: true },
-  { id: 'e22a', source: '2', target: '2a', type: edgeType, animated: true },
-  { id: 'e22b', source: '2', target: '2b', type: edgeType, animated: true },
-  { id: 'e22c', source: '2', target: '2c', type: edgeType, animated: true },
-  { id: 'e2c2d', source: '2c', target: '2d', type: edgeType, animated: true },
-  { id: 'e45', source: '4', target: '5', type: edgeType, animated: true },
-  { id: 'e56', source: '5', target: '6', type: edgeType, animated: true },
-  { id: 'e57', source: '5', target: '7', type: edgeType, animated: true },
+  { id: 'e12', source: '1', target: '2'},
+  { id: 'e13', source: '1', target: '3'},
+  { id: 'e22a', source: '2', target: '2a' },
+  { id: 'e22b', source: '2', target: '2b'},
+  { id: 'e22c', source: '2', target: '2c' },
+  { id: 'e2c2d', source: '2c', target: '2d' },
+  { id: 'e45', source: '4', target: '5' },
+  { id: 'e56', source: '5', target: '6' },
+  { id: 'e57', source: '5', target: '7' },
 ];
 
 //Антон Pepormaty, [10/28/2025 3:56 PM]
